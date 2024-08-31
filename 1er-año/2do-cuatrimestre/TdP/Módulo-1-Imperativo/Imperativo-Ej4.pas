b3abc92a8ff164c7dd03273f52eebbbac6b2a946
Gonzalo <111930186+geonnn@users.noreply.github.com>
2024-08-31 17:22:32 -0300

    TdP | Imperativo-Ej4

--- 1er-año/2do-cuatrimestre/TdP/Módulo-1-Imperativo/Imperativo-Ej4.pas
+++ 1er-año/2do-cuatrimestre/TdP/Módulo-1-Imperativo/Imperativo-Ej4.pas
@@ -8,3 +8,162 @@ e. Muestre los precios del vector resultante del punto d).
 f. Calcule el promedio de los precios del vector resultante del punto d).}
 
 program Ej4;
+type
+    recProducto = record
+        codProd: integer;
+        codRubro: 1..8;
+        precio: real;
+    end;
+
+    listaProductos = ^nodo;
+    nodo = record
+        dato: recProducto;
+        sig: listaProductos;
+    end;
+
+    vecListas = array[1..8] of listaProductos;
+    vecRub3 = array[1..30] of recProducto;
+
+procedure LeerProducto(var p: recProducto);
+begin
+    writeln('Ingrese precio del producto: ');
+    readln(p.precio);
+    if (p.precio = 0) then
+        writeln('Carga finalizada.')
+    else begin
+        writeln('Ingrese código del producto: ');
+        readln(p.codProd);
+        writeln('Ingrese código de rubro (del 1 al 8): ');
+        readln(p.codRubro);
+    end;
+end;
+
+procedure InsertarOrdenado(p: recProducto; var L: listaProductos);
+var
+    act, nue, ant: listaProductos;
+begin
+    new(nue);
+    nue^.dato := p;
+    act := L;
+    while (act <> nil) and (p.codProd > act^.dato.codProd) do begin
+        ant := act;
+        act := act^.sig;
+    end;
+    if (act = L) then
+        L := nue
+    else
+        ant^.sig := nue;
+    nue^.sig := act;
+end;
+
+// Hay una lista en cada posición del vector. Se envía por parámetro a InsertarOrdenado la lista que está en la posición del vector correspondiente al rubro del producto que ingresa.
+procedure CargarVecL(var vL: vecListas);
+var
+    p: recProducto;
+begin
+    LeerProducto(p);
+    while (p.precio <> 0) do begin
+        InsertarOrdenado(p, vL[p.codRubro]);
+        LeerProducto(p);
+    end;
+end;
+
+procedure InicializarVecL(var vL: vecListas);
+var
+    i: integer;
+begin
+    for i := 1 to 8 do
+        vL[i] := nil;
+end;
+
+procedure CargarVecR3(p: recProducto; var vR3: vecRub3; var vR3_dimL: integer);
+begin
+    vR3_dimL := vR3_dimL + 1;
+    vR3[vR3_dimL] := p;
+end;
+
+procedure RecorrerLista(L: listaProductos; r3: boolean; var vR3: vecRub3; var vR3_dimL: integer);
+begin
+    if (r3) then // si r3 es verdadero se carga el vector y se imprime, sino, sólo se imprime.
+        while (L <> nil) and (vR3_dimL < 30) do begin // El vector sólo puede cargar hasta 30 productos. 
+            CargarVecR3(L^.dato, vR3, vR3_dimL);
+            writeln(L^.dato.codProd);
+            L := L^.sig;
+        end
+    else
+        while (L <> nil) do begin
+            writeln(L^.dato.codProd);
+            L := L^.sig;
+        end;
+end;
+
+procedure RecorrerVecL(vL: vecListas; var vR3: vecRub3; var vR3_dimL: integer);
+var
+    i: integer;
+    r3: boolean;
+begin
+    for i := 1 to 8 do begin
+        r3 := false;
+        if (i = 3) then // Para no recorrer dos veces la lista: si es el rubro 3 se pone r3 en true.
+            r3 := true;
+        writeln('');
+        writeln('Rubro ', i, ':');
+        RecorrerLista(vL[i], r3, vR3, vR3_dimL); // Se envía por valor r3.
+        writeln('');
+    end;
+end;
+
+procedure OrdenarVecR3(var vR3: vecRub3; vR3_dimL: integer);
+var
+    i, j, pos: integer;
+    item: recProducto;
+begin
+    for i := 1 to (vR3_dimL - 1) do begin
+        pos := i;
+        for j := i + 1 to vR3_dimL do
+            if (vR3[j].precio < vR3[pos].precio) then
+                pos := j;
+        item := vR3[pos];
+        vR3[pos] := vR3[i];
+        vR3[i] := item;
+    end;
+end;
+
+function Promedio(cant: integer; total: real):real;
+begin
+    Promedio := total / cant;
+end;
+
+procedure ImprimirVector(vR3: vecRub3; vR3_dimL: integer; var promRub3: real);
+var
+    i: integer;
+    total: real;
+begin
+    total := 0;
+    writeln('Precios rubro 3:');
+    for i := 1 to vR3_dimL do begin
+        writeln(i, '. ', 'Código ', vR3[i].codProd, ':   $', vR3[i].precio:0:2);
+        total := total + vR3[i].precio;
+    end;
+    promRub3 := Promedio(vR3_dimL, total);
+end;
+
+var
+    vR3: vecRub3;
+    vL: vecListas;
+    vR3_dimL: integer;
+    promRub3: real;
+begin
+    promRub3 := 0;
+    vR3_dimL := 0;
+    InicializarVecL(vL);
+    CargarVecL(vL);
+    RecorrerVecL(vL, vR3, vR3_dimL);
+    OrdenarVecR3(vR3, vR3_dimL);
+    writeln('');
+    ImprimirVector(vR3, vR3_dimL, promRub3);
+    writeln('');
+    writeln('vr3_diml: ', vr3_diml);
+    writeln('');
+    writeln('Promedio de precios rubro 3: ', promRub3:0:2);
+end.
