88a3605168482ca8ff5beb42956d9856e2a04db1
Gonzalo <111930186+geonnn@users.noreply.github.com>
2025-03-27 12:52:51 -0300

    FOD | Práctica 1 ejs. 5-6

--- 2do-año/1er-cuatrimestre/FOD/Prácticas/Práctica 1/Ej5-6/ej5.pas
+++ 2do-año/1er-cuatrimestre/FOD/Prácticas/Práctica 1/Ej5-6/ej5.pas
@@ -29,7 +29,7 @@ type
 procedure PrintCelular(c: recCelular);
 begin
     writeln('Código: ', c.cod,
-            ' | Precio: $', c.precio,
+            ' | Precio: $', c.precio:0:2,
             ' | Marca: ', c.marca,
             ' | Stock disp.: ', c.stDisp,
             ' | Stock mín.: ', c.stMin,
@@ -94,23 +94,29 @@ begin
     writeln('Ingrese la ruta del archivo a crear: ');
     readln(nomArch);
     assign(arch, nomArch);
-    rewrite(arch);
+    //rewrite(arch);
 end;
 
 procedure CrearArchivo(var arch: archivo);
 var
     carga: Text;
     c: recCelular;
-    st: string;
 begin
     assign(carga, 'celulares.txt');
     reset(carga);
     rewrite(arch);
     while not EOF(carga) do begin
-        readln(carga, st);
-        writeln(st);
-        // write(arch, c);
+        readln(carga, c.cod, c.precio, c.marca);
+        c.marca := Trim(c.marca);
+        
+        readln(carga, c.stDisp, c.stMin, c.desc);
+        c.desc := Trim(c.desc);
+
+        readln(carga, c.nom);
+
+        write(arch, c);
     end;
+    writeln('Archivo creado correctamente.');
     close(carga);
 end;
 
@@ -140,6 +146,13 @@ begin
     end;
 end;
 
+procedure EscribirEnTexto(c: recCelular; var arch_texto: Text);
+begin
+    writeln(arch_texto, c.cod, ' ', c.precio:0:2, ' ', c.marca);
+    writeln(arch_texto, c.stDisp, ' ', c.stMin, ' ', c.desc);
+    writeln(arch_texto, c.nom);
+end;
+
 procedure Exportar(var arch: archivo);
 var
     c: recCelular;
@@ -149,29 +162,83 @@ begin
     rewrite(arch_texto);
     while not EOF(arch) do begin
         read(arch, c);
-        write(arch_texto, 'Código: ', c.cod, ' | Precio: $', c.precio, ' | Marca: ', c.marca);
-        write(arch_texto, 'Stock disp.: ', c.stDisp, ' | Stock mín.: ', c.stMin, ' | Desc.: ', c.desc);
-        write(arch_texto, 'Nombre: ', c.nom);
+        EscribirEnTexto(c, arch_texto);
     end;
+    writeln('Archivo exportado correctamente.');
     close(arch_texto);
 end;
 
 procedure AgregarCelular(var arch: archivo);
+
+    function CodExiste(cod: integer; var arch: archivo): boolean;
+    var
+        c: recCelular;
+    begin
+        seek(arch, 0);
+        while (not EOF(arch)) and (cod <> c.cod) do
+            read(arch, c);
+        if (cod = c.cod) then
+            CodExiste := true
+        else
+            CodExiste := false;
+    end;
+
 var
     c: recCelular;
 begin
+    LeerCelular(c);
+    while (c.cod <> -1) do begin
+        if not CodExiste(c.cod, arch) then begin
+            write(arch, c);
+            writeln;
+            writeln('Celular añadido correctamente.');
+        end
+        else begin
+            writeln;
+            writeln('El celular con el código ingresado ya existe.');
+        end;
+        LeerCelular(c)
+    end;
+
 end;
 
 procedure ModificarStockDisp(var arch: archivo);
 var
     c: recCelular;
+    stock: integer;
+    nom: string;
 begin
+    writeln('Ingrese nombre de celular para modificar su stock: ');
+    readln(nom);
+    read(arch, c);
+    while (not EOF(arch)) and (nom <> c.nom) do
+        read(arch, c);
+    if (nom = c.nom) then begin
+        writeln('Ingrese nuevo valor de stock para el celular: ');
+        readln(stock);
+        c.stDisp := stock;
+        seek(arch, FilePos(arch)-1);
+        write(arch, c);
+        writeln('Stock modificado correctamente.');
+    end
+    else
+        writeln('No se encontró el celular ', nom, '.');
 end;
 
 procedure ExportarSinStock(var arch: archivo);
 var
     c: recCelular;
+    sinStock: Text;
 begin
+    assign(sinStock, 'SinStock.txt');
+    rewrite(sinStock);
+    while not EOF(arch) do begin
+        read(arch, c);
+        if (c.stDisp = 0) then
+            EscribirEnTexto(c, sinStock);
+    end;
+    writeln('Archivo exportado correctamente.');
+    close(sinStock);
 end;
 
 var
@@ -184,18 +251,20 @@ begin
         TextoMenu;
         reset(archivo_logico);
         readln(m);
+        writeln;
         case (m) of
             1: CrearArchivo(archivo_logico);
             2: ListarStockBajo(archivo_logico);
             3: ListarDesc(archivo_logico);
             4: Exportar(archivo_logico);
-            // 5: AgregarCelular(archivo_logico);
-            // 6: ModificarStockDisp(archivo_logico);
-            // 7: ExportarSinStock(archivo_logico);
+            5: AgregarCelular(archivo_logico);
+            6: ModificarStockDisp(archivo_logico);
+            7: ExportarSinStock(archivo_logico);
             0: writeln(Separador('Programa finalizado'));
         else writeln('Ingrese una opción válida.');
         end;
-        VolverMenu;
+        if (m <> 0) then
+            VolverMenu; // Espera input para volver al menu.
+        close(archivo_logico);
     until (m = 0);
-    close(archivo_logico);
 end.
