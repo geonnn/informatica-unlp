48b91377e807e9d9899c2bf9dfa652855707683a
Gonzalo <111930186+geonnn@users.noreply.github.com>
2025-06-28 10:21:45 -0300

    AyED | Grafos

---
+++ 2do-año/1er-cuatrimestre/AyED/Prácticas/ayed/ayed_grafos/src/tp5/ejercicio2/Recorridos.java
@@ -0,0 +1,20 @@
+package tp5.ejercicio2;
+
+import java.util.*;
+import tp5.ejercicio1.*;
+
+public class Recorridos<T> {
+	
+	public List<T> dfs(Graph<T> grafo) {
+		List<T> lista = new LinkedList<T>();
+		
+		return lista;
+	}
+	
+	public List<T> bfs(Graph<T> grafo) {
+		List<T> lista = new LinkedList<T>();
+		
+		return lista;
+	}
+
+}
